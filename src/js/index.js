8b9aa2c752ad9a67137b16d208a8b19ff7923e86
VladTarGalanix <vlad.galanix@gmail.com>
2017-09-04 21:53:53 +0300

    Implement chart axises and scales, add dots and tooltip when hoveringover dots

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,8 @@ const model = {
     updateFrequency: 60000,
     currentPriceData: {},
     historicalPriceData: {},
-    historyGraphWidth: 300,
-    historyGraphHeight: 150,
+    historyGraphWidth: 400,
+    historyGraphHeight: 250,
     startFetchingData() {
         this.requestCurrentPriceData();
         this.intervalID = setInterval(() => {
@@ -49,14 +49,15 @@ const historyGraphView = {
     init() {
         this.graphSVG = d3.select('.graph').append('svg');
     },
-    renderGraph({ width, height, data }) {    
+    renderGraph({ width, height, data }) {
+        // transforms a string into a Date object
         const createDateObj = (dateStr) => {
             const dateArr = dateStr.split('-');
             const year = dateArr[0];
             const month = dateArr[1];
             const day =  dateArr[2];
             return new Date(year, month, day);
-        }        
+        }
         // create an array(dataset) from an object(data)
         const dataset = [];
         const keys = Object.keys(data);
@@ -66,40 +67,93 @@ const historyGraphView = {
                 currencyValue: data[key]
             });
         });
-
+        this.buildLine({
+            dataset,
+            width,
+            height
+        });
+    },
+    buildLine({ dataset, width, height }) {
+        const firstDate = dataset[0].time.getTime();
+        const lastDate = dataset[dataset.length - 1].time.getTime();
         const xScale = d3.scaleLinear()
                          .domain([
-                             d3.min(dataset, d => d.time.getTime()),
-                             d3.max(dataset, d => d.time.getTime())
+                           firstDate,
+                           lastDate                           
                          ])
                          .range([0, width]);
 
         const yScale = d3.scaleLinear()
                          .domain([
-                             0,
-                             d3.max(dataset, d => d.currencyValue)
+                           d3.min(dataset, d => d.currencyValue),
+                           d3.max(dataset, d => d.currencyValue)
                          ])
                          .range([height, 0]);
-        
+
         const lineFunction = d3.line()
                                .x(d => xScale(d.time.getTime()))
                                .y(d => yScale(d.currencyValue))
-                               .curve(d3.curveBasis);
-
-
-                                 
+                               .curve(d3.curveBasis);                                       
+                
+        // constructed basic graph
         this.graphSVG
+          .attrs({
+            width,
+            height
+          })
+          .append('path')
             .attrs({
-                width,
-                height
-            })
-            .append('path')
-              .attrs({
-                'd': lineFunction(dataset),
-                'stroke': '#F51455',
-                'stroke-width': 2,
-                'fill': 'none'
+              'd': lineFunction(dataset),
+              'stroke': '#C2390D',
+              'stroke-width': 2,
+              'fill': 'none'
             });
+
+        // add axises
+        const yAxisGen = d3.axisLeft(yScale).ticks(4);
+        const xAxisGen = d3.axisBottom(xScale).tickFormat(d3.timeFormat('%e')).ticks(dataset.length);
+        
+        const padding = 20; // random number
+
+        const yAxis = this.graphSVG
+                        .append('g')
+                        .call(yAxisGen)
+                        .attr('transform', `translate(${padding}, ${-padding * (1.15)})`);
+
+        const xAxis = this.graphSVG
+                        .append('g')
+                        .call(xAxisGen)
+                        .attr('transform', `translate(${padding}, ${height - padding})`);
+
+        // dots represent single smallest time duration        
+        const tooltip = d3.select('body').append('div')
+                          .attr('class', 'tooltip')
+                          .style('opacity', 0);
+
+        const dots = this.graphSVG.selectAll('circle')
+                       .data(dataset)
+                       .enter()
+                       .append('circle')
+                         .attrs({
+                            cx: d => xScale(d.time.getTime()),
+                            cy: d => yScale(d.currencyValue),
+                            r: 5,
+                            'fill': '#32B9B5'
+                          }) // add tooltip on hover
+                       .on('mouseover', function(d) {                 
+                         tooltip.transition()
+                           .duration(500)
+                           .style('opacity', 0.75)
+                         tooltip.html(`<strong>Price: $${d.currencyValue}</strong>`)
+                           .style('left', (d3.event.pageX) + 'px')
+                           .style('top', (d3.event.pageY) + 'px')                     
+                       }) // remove tooltip on hover
+                       .on('mouseout', () => {
+                         tooltip.transition()
+                           .duration(300)
+                           .style('opacity', 0);
+                       })
+
     }
 }
 
