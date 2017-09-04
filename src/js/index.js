c814588f2d0b32fbf879bc66e2b8e1b3a50a42f9
VladTarGalanix <vlad.galanix@gmail.com>
2017-09-04 17:27:23 +0300

    Finish implementing data fetching and updating logic

--- src/js/index.js
+++ src/js/index.js
@@ -3,34 +3,53 @@ import '../scss/index.scss';
 const model = {
     currentPriceURL: 'https://api.coindesk.com/v1/bpi/currentprice.json',
     updateFrequency: 60000,
+    currentPriceData: {},
     startFetchingData() {
         this.requestCurrentPriceData();
-        setTimeout(this.requestCurrentPriceData, this.updateFrequency);
+        this.intervalID = setInterval(() => {
+            this.requestCurrentPriceData.bind(this);
+            console.log('boom, updated');
+        }, this.updateFrequency);        
     },
-    requestCurrentPriceData() {
+    requestCurrentPriceData() {      
         fetch(this.currentPriceURL)
             .then(response => response.json())
             .then(data => {
                 this.currentPriceData = data;
-                return data;
-            })
-            .then(sameData => console.log(this.currentPriceData))
+                controller.renderCurrentPrice();
+            })          
             .catch(error => console.warn(error));
     }
-}
+};
 
 const currentPriceView = {
-    init() {}
-}
+    init() {
+        this.valueHolderUSD = document.querySelector('.current-price__USD .value');
+        this.valueHolderEUR = document.querySelector('.current-price__EUR .value');        
+        this.euroSign = '&euro;'
+        this.dollarSign = '&#36;'
+    },
+    renderData({ rateUSD, rateEUR }) {
+        this.valueHolderUSD.innerHTML = this.dollarSign + rateUSD;
+        this.valueHolderEUR.innerHTML = this.euroSign + rateEUR;
+    }
+
+};
 
 const controller = {
     init() {
         model.startFetchingData();
         currentPriceView.init();
     },
-    getCurrentPriceData() {
-        return model.currentPriceData;
+    renderCurrentPrice() {
+      const rateEUR = model.currentPriceData.bpi.EUR.rate;
+      const rateUSD = model.currentPriceData.bpi.USD.rate;
+      currentPriceView.renderData({
+          rateEUR,
+          rateUSD
+      });
     }
+
 };
 
 controller.init();
