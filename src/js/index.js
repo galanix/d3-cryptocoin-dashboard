1828435b7dcce3e5ead2557ddbe3d355a4506622
VladTarGalanix <vlad.galanix@gmail.com>
2017-09-04 19:50:00 +0300

    Implement fetching of historical bitcoin prices and rendering of basic graph(line) based on fetched data

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,19 @@ import '../scss/index.scss';
 
 const model = {
     currentPriceURL: 'https://api.coindesk.com/v1/bpi/currentprice.json',
+    historicalPriceURL: 'https://api.coindesk.com/v1/bpi/historical/close.json',
     updateFrequency: 60000,
     currentPriceData: {},
+    historicalPriceData: {},
+    historyGraphWidth: 300,
+    historyGraphHeight: 150,
     startFetchingData() {
         this.requestCurrentPriceData();
         this.intervalID = setInterval(() => {
             this.requestCurrentPriceData.bind(this);
-            console.log('boom, updated');
         }, this.updateFrequency);        
     },
-    requestCurrentPriceData() {      
+    requestCurrentPriceData() {     
         fetch(this.currentPriceURL)
             .then(response => response.json())
             .then(data => {
@@ -19,6 +22,12 @@ const model = {
                 controller.renderCurrentPrice();
             })          
             .catch(error => console.warn(error));
+    },
+    requestHistoricalPriceData() {
+        d3.json(this.historicalPriceURL, (data) => {            
+            this.historicalPriceData = data;
+            controller.renderHistoryGraph()
+        })
     }
 };
 
@@ -36,10 +45,71 @@ const currentPriceView = {
 
 };
 
+const historyGraphView = {
+    init() {
+        this.graphSVG = d3.select('.graph').append('svg');
+    },
+    renderGraph({ width, height, data }) {    
+        const createDateObj = (dateStr) => {
+            const dateArr = dateStr.split('-');
+            const year = dateArr[0];
+            const month = dateArr[1];
+            const day =  dateArr[2];
+            return new Date(year, month, day);
+        }        
+        // create an array(dataset) from an object(data)
+        const dataset = [];
+        const keys = Object.keys(data);
+        keys.forEach(key => {    
+            dataset.push({
+                time: createDateObj(key),
+                currencyValue: data[key]
+            });
+        });
+
+        const xScale = d3.scaleLinear()
+                         .domain([
+                             d3.min(dataset, d => d.time.getTime()),
+                             d3.max(dataset, d => d.time.getTime())
+                         ])
+                         .range([0, width]);
+
+        const yScale = d3.scaleLinear()
+                         .domain([
+                             0,
+                             d3.max(dataset, d => d.currencyValue)
+                         ])
+                         .range([height, 0]);
+        
+        const lineFunction = d3.line()
+                               .x(d => xScale(d.time.getTime()))
+                               .y(d => yScale(d.currencyValue))
+                               .curve(d3.curveBasis);
+
+
+                                 
+        this.graphSVG
+            .attrs({
+                width,
+                height
+            })
+            .append('path')
+              .attrs({
+                'd': lineFunction(dataset),
+                'stroke': '#F51455',
+                'stroke-width': 2,
+                'fill': 'none'
+            });
+    }
+}
+
 const controller = {
     init() {
+        historyGraphView.init();
+        model.requestHistoricalPriceData();
         model.startFetchingData();
         currentPriceView.init();
+        
     },
     renderCurrentPrice() {
       const rateEUR = model.currentPriceData.bpi.EUR.rate;
@@ -48,6 +118,17 @@ const controller = {
           rateEUR,
           rateUSD
       });
+    },
+    renderHistoryGraph() {
+       //model.historicalPriceData
+       const width = model.historyGraphWidth;
+       const height = model.historyGraphHeight;
+       const data = model.historicalPriceData.bpi; // object
+       historyGraphView.renderGraph({
+           width,
+           height,
+           data
+       });
     }
 
 };
