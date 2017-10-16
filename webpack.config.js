8e23846c3939da41e22e5a64b0c3e0eae450c317
VladTarGalanix <vlad.galanix@gmail.com>
2017-10-16 18:37:55 +0300

    change from development build to the production one by adding minifiers

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,58 @@
 const path = require('path');
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
     entry: {
-      'bundle': './src/js/index.js',     
+      'bundle': './src/js/index.js',
     },
     output: {
-        filename: '[name].js',
+        filename: '[name].min.js',
         path: path.resolve(__dirname, 'dest'),
         publicPath: 'dest/'
     },
     module: {
         rules: [
-        {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                  presets: [
-                    'es2015', 'react'
-                  ]
-              }
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        'es2015', 'react'
+                    ]
+                }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [
+                    { loader: 'css-loader', options: { minimize: true} },
+                    { loader: 'postcss-loader' },
+                    { loader: 'sass-loader'}
+                ]
+                }),
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ['css-loader','postcss-loader']
+                })
             }
-        },
-        {
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-              fallback: "style-loader",
-              use: ['css-loader','postcss-loader','sass-loader']
-            }),
-        },
-        {       
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-              fallback: "style-loader",
-              use: ['css-loader','postcss-loader']
-            })
-        }
         ]
     },
     plugins: [
-        new ExtractTextPlugin("bundle.css"),
+        new ExtractTextPlugin("bundle.min.css"),
+        new UglifyJSPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+              NODE_ENV: JSON.stringify('production')
+            }
+          }),
     ]
-}
+};
