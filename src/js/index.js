757767c6b3b00219778ccba491519f67a2d8db95
VladTarGalanix <vlad.galanix@gmail.com>
2017-10-19 17:27:33 +0300

    Separate imports and add comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,24 @@
+// STYLES
 import "../scss/index.scss";
 
+//  JQUERY & BOOTSTRAP
+import $ from "jquery";
+import "bootstrap";
+
+// POLYFILLS
+import "./polyfills/requestAnimationFrame";
+
+// REACT
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./reactComponents/App";
 
+// REDUX
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-
 import reducers from "./reduxComponents/reducers";
-import App from "./reactComponents/App";
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
