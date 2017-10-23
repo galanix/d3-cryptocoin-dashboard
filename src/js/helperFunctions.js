c92c6f26c04bb36be620e4ffacfcc476211fc44e
VladTarGalanix <vlad.galanix@gmail.com>
2017-10-23 14:23:47 +0300

    Add function that compares if two arrays have the same items - towArraysAreEqual()

--- src/js/helperFunctions.js
+++ src/js/helperFunctions.js
@@ -55,6 +55,30 @@ export function removeDuplicates(array) {
     return a;
 }
 
+export function twoArraysAreEqual(array, array2) {
+    // if the other array is a falsy value, return
+    if (!array2)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (array.length != array2.length)
+        return false;
+
+    for (var i = 0, l=array.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (array[i] instanceof Array && array2[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!array[i].equals(array2[i]))
+                return false;       
+        }           
+        else if (array[i] != array2[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;    
+}
+
 // recursivly finds averages
 export function formTickValues({ finalLevel, level, prevSm, prevLg }) {
     let outputArray = [ prevSm, prevLg ];
