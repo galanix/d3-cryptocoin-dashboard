ff5db893cf4db0208f8294da6979360b0453c115
VladTarGalanix <vlad.galanix@gmail.com>
2017-10-25 17:53:09 +0300

    change Chart.js children rendering approach

--- src/js/reactComponents/Dashboard/CryptoBoard/children/children/Chart.js
+++ src/js/reactComponents/Dashboard/CryptoBoard/children/children/Chart.js
@@ -9,42 +9,28 @@ import BarChart from "./children/chartTypes/BarChart.js";
 export default class Chart extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = { ChartJSX: null };
     }
     renderChart(type, comparisionField) {
-        const width = Math.round(this.svgDiv.getBoundingClientRect().width);
-        const height = Math.round(width / 2);
-        const keys = Object.keys(this.props.hashTable);
-        const dataset = keys.map(key => this.props.hashTable[key]);
-        const colorValues = keys.map(key => this.props.hashTable[key].color);
-        let deletePrevSvg = false;
-
-        this.setState(prevState => ({
-            color: d3.scaleOrdinal(colorValues),
-            dataset,
-            width,
-            height,
-            comparisionField,
-            type: type
-        }));
-        
-    }
-    render() {
-      let props;
+      const width = Math.round(this.svgDiv.getBoundingClientRect().width);
+      const height = Math.round(width / 2);
+      const keys = Object.keys(this.props.hashTable);
+      const dataset = keys.map(key => this.props.hashTable[key]);
+      const colorValues = keys.map(key => this.props.hashTable[key].color);
+      const color = d3.scaleOrdinal(colorValues);
+      
       let ChartJSX = null;
-
-      if(!!this.state) {
-        props = {
-          color: this.state.color,
-          dataset: this.state.dataset,
-          width: this.state.width,
-          height: this.state.height,
-          comparisionField: this.state.comparisionField,
-          chartIsDonut: this.state.type === "pie-donut",
-          type: this.state.type,
-        };
-      }
-      switch(this.state.type) {
+      const props = {
+          color: color.bind(this),
+          dataset,
+          width,
+          height,
+          comparisionField,
+          chartIsDonut: type === "pie-donut",
+          type: type,
+      };
+      
+      switch(type) {
         case "pie":
         case "pie-donut":
           ChartJSX = ( <PieChart {...props} /> );
@@ -55,9 +41,15 @@ export default class Chart extends React.Component {
         default:
           console.warn("chart has not been rendered");
       }
+
+      this.setState({
+        ChartJSX
+      });     
+    }
+    render() {
       return (
         <div ref={div => this.svgDiv = div} className="graph">
-            { ChartJSX }
+            { this.state.ChartJSX }
         </div>
       );
     }
