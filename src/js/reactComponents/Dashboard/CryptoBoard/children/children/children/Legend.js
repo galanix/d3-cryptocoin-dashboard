cdf7b3d4d68f60e663fe06958b52a912a295f435
VladTarGalanix <vlad.galanix@gmail.com>
2017-10-25 17:57:01 +0300

    Make Legend section totally rerender with each update

--- src/js/reactComponents/Dashboard/CryptoBoard/children/children/children/Legend.js
+++ src/js/reactComponents/Dashboard/CryptoBoard/children/children/children/Legend.js
@@ -1,31 +1,30 @@
 import React from "react";
 import * as d3 from "d3";
 
-export default class extends  React.Component {
+export default class extends React.Component {
     build() {
-        // { dataset, comparisionField, onHoverHandler }
-        let index = 0;
         const legend = d3.select(this.legend);
         
-        legend.selectAll("*").remove(); // remove old legend items
+        legend.selectAll(".legend_item").remove();
+
         const items = legend.selectAll(".legend_item")
-          .data(this.props.dataset) // add new legend items
+          .data(this.props.dataset)
           .enter()
           .append("div")
-          .attrs({
-            "data-index": () => index++,
-            "class": "legend_item",
-          })
-          .on("mouseover", d => this.props.onHoverHandler(1, this.props.color(d[this.props.comparisionField]), d, this.props.comparisionField ))
-          .on("mouseout", d => this.props.onHoverHandler(0, "#333", d, this.props.comparisionField));        
+          .attr("data-currency-id", d => d.id)
+          .attr("class", "legend_item")       
+          .on("mouseover", d => this.props.onHoverHandler(1, this.props.color(d[this.props.comparisionField]), d))
+          .on("mouseout", d => this.props.onHoverHandler(0, "#333", d));
+        
         items
           .append("span")
           .attr("class", "square")
           .style("background-color", d => this.props.color(d[this.props.comparisionField]));
-    
+        
         items
           .append("span")
-          .text(d => d.name);
+          .text(d => d.name)
+          .style("font-weight", "bold");
     }
     render() {
         return (
