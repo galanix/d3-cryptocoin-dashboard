fa58653e193deb0826ba5a939c8ca908ca49e967
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-01 21:08:19 +0200

    Let Horizontal Bar Chart display negative values properly

--- src/js/reactComponents/Dashboard/CryptoBoard/children/children/children/chartTypes/HBarChart.js
+++ src/js/reactComponents/Dashboard/CryptoBoard/children/children/children/chartTypes/HBarChart.js
@@ -7,7 +7,10 @@ import {twoArraysAreEqual} from "../../../../../../../helperFunctions.js";
 export default class HBarChart extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            duration: 300,
+            margin: { top: 30, right: 10, bottom: 30, left: 70 }
+        };
     }
     componentDidMount() {
         this.renderSVG();      
@@ -29,22 +32,22 @@ export default class HBarChart extends React.Component {
         this.updateSVG();
     }
     renderSVG() {
-        const margin = { top: 30, right: 100, bottom: 30, left: 100 };
+        const margin = this.state.margin;
         const fixedWidth = this.props.width - (margin.left + margin.right);
         const fixedHeight = this.props.height - (margin.top + margin.bottom);
+        const svg = d3.select(this.svg);        
+
         this.setState({fixedHeight}); // will need this variable later on
-        const svg = d3.select(this.svg);
   
         svg.attr("width", this.props.width)
             .attr("height", this.props.height)
 
-        this.yScale = d3.scalePoint()
-            .range([0, fixedHeight])
-            .padding(0.2);
+        this.yScale = d3.scaleBand()
+            .range([fixedHeight, 0])
+            .padding(0.1);
 
         this.xScale = d3.scaleLinear()
-            .range([0, fixedWidth])
-            .nice();
+            .range([0, fixedWidth]);
 
         this.setState({
             g: svg.append("g")
@@ -56,24 +59,59 @@ export default class HBarChart extends React.Component {
                 this.state.g.append("g")
                     .attr("class", "axis axis--y");
                 
+                this.state.g.append("g")
+                    .attr("class", "axis axis--x");
+
                 this.updateSVG();
-            });        
+            });
     }
     updateSVG() {
         const {dataset, comparisionField, type} = this.props;
         const g = this.state.g;
-        let [min, max] = d3.extent(dataset, d => +d[comparisionField]);
-        min = Math.min(min, 0);
-        max = Math.max(max, 0);
+        const {fixedWidth, fixedHeight, duration} = this.state;        
+
+        let [min, max] = d3.extent(dataset, d => +d[comparisionField]);       
 
         this.yScale.domain(dataset.map(d => d.id));
         this.xScale.domain([min, max]);
+        
+        // ADD Y AXIS
+        const inRange = val => {
+            if(val < 0) {
+                return 0;
+            }
+            if(val > fixedWidth) {
+                return fixedWidth;                
+            }
+            return val;
+        }
 
-        // ADD LABELS WITH CURRENCY NAMES
-        this.state.g.select(".axis--y")
+        const yAxis = g.select(".axis--y");
+        
+        yAxis.transition()
+            .duration(duration)
+            .attr("transform", `translate(${inRange(this.xScale(0))}, 0)`) //  > 0 ? this.xScale(0) : 0 
+            .call(d3.axisLeft(this.yScale).tickValues(dataset.map(d => d.id)));
+
+        yAxis.selectAll(".tick")
+            .data(dataset)
+            .select("text")
             .transition()
-            .duration(300)
-            .call(d3.axisLeft(this.yScale).tickValues(dataset.map(d => d.id)));        
+            .duration(duration)
+            .attr("x", d => +d[comparisionField] < 0 ? 10 : -10)
+            .style("font-size", "14px")
+            .style("text-anchor", d => {
+                console.log(+d[comparisionField])
+                return +d[comparisionField] < 0 ? "start" : "end";
+            });
+
+        // ADD X AXIS
+        g.select(".axis--x")
+            .attr("transform", `translate(0, ${fixedHeight})`)
+            .transition()
+            .duration(duration)
+            .call(d3.axisBottom(this.xScale).ticks(5));
+
        
         // APPEND RECTANGLES
         const rects = g.selectAll("rect")
@@ -82,46 +120,39 @@ export default class HBarChart extends React.Component {
         rects.exit()
             .remove();
 
-        let bandwidth =  (this.state.fixedHeight / dataset.length);
-            bandwidth *= 0.9;
-
         rects.enter()
             .append("rect")
             .merge(rects)
-                .on("mouseover", d => this.toggleBar(d3.event.target.getAttribute("data-currency-id"), d, false))
-                .on("mouseout", d => this.toggleBar(d3.event.target.getAttribute("data-currency-id"), d, true))
+                .on("mouseover", d => this.toggleBar(d3.event.target, d, false))
+                .on("mouseout", d => this.toggleBar(d3.event.target, d, true))
                 .transition()
-                .duration(300)
+                .duration(duration)
                 .attrs({
                     "fill":  d => this.props.color(+d[comparisionField]),
-                    "data-currency-id": d => d.id,
-                    "height": () => bandwidth > 200 ? 200 : bandwidth,
-                    "y": d => this.yScale(d.id) - bandwidth / 2,
-                    "width": d => Math.abs(this.xScale(+d[comparisionField]) - (this.xScale(0))),
-                    "x": this.xScale(min),
+                    "data-currency-id": d => d.id, // ???
+                    "height": () => this.yScale.bandwidth(),
+                    "y": d => this.yScale(d.id),
+                    "width": d => Math.abs(this.xScale(+d[comparisionField]) - this.xScale(0)),
+                    "x": d => {
+                        let val = 0;
+                        if(min > 0) {
+                            val = min;
+                        }
+                        if(max < 0) {
+                            val = max;
+                        }
+                        return this.xScale(Math.min(val, +d[comparisionField]))
+                    }
                 });
 
-        // APPEND CURRENCY VALUES TO THE RIGHT
-        const text = g.selectAll(".value")
-            .data(dataset);
-        
-        text.exit().remove();
-
-        text.enter()
-            .append("text")
-            .merge(text)
-                .attr("class", "value")
-                .attr("y", d => this.yScale(d.id) + 7) // font === 14px, 14 / 2 === 7
-                .attr("x", d => Math.abs(this.xScale(+d[comparisionField]) - (this.xScale(0))) + 5)
-                .style("font-size", "14px")
-                .text(d => d[comparisionField]);
+        console.log(yAxis.selectAll(".tick").style("font-size"));
     }
-    toggleBar() {
-        // darken the rect?       
+    toggleBar(el, d, mouseOut) {
+        // show the exact value
     }
     render() {
-        return (            
-              <svg ref={svg => this.svg = svg}></svg>            
+        return (
+              <svg ref={svg => this.svg = svg}></svg>
         );
     }
 }
