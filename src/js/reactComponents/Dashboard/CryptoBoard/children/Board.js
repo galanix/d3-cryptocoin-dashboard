85839a5eb56940260dc0c97fc5a1298c48ce0b8d
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-06 13:17:22 +0200

    Refactor methods

--- src/js/reactComponents/Dashboard/CryptoBoard/children/Board.js
+++ src/js/reactComponents/Dashboard/CryptoBoard/children/Board.js
@@ -9,20 +9,23 @@ export default class Board extends React.Component {
         this.state = {
             componentToUpdate: "CryptoBoard_table",            
         };
+        this.saveChanges = this.saveChanges.bind(this);
+        this.updateTable = this.updateTable.bind(this);
+        this.clearFilters = this.clearFilters.bind(this)
     }
     componentDidMount() {
         const { limit, currency } = this.props.model.filters;
         this.props.update(this.props.createURL(limit, currency), this.state.componentToUpdate)
-          .then(() => this.setState({
-              filteredData: this.props.model.data
-          }));
+            .then(() => this.setState({
+                filteredData: this.props.model.data
+            }));
     }
-    changeFilter(filterName, target) {
+    changeFilter(callback, filterName, target) {
         const newFilterValue = target.getAttribute("data-value");
         
         this.props.change(newFilterValue, filterName, this.state.componentToUpdate);
-             
-        this.updateTable();        
+        
+        callback();
     }
     updateTable() {
         // UPDATES THE TABLE
@@ -31,27 +34,32 @@ export default class Board extends React.Component {
 
         // helper functions
         const defineConstraints = (key, vals) => {
-          let min;
-          let max;
-          if(vals[key] instanceof Array) {
-            min = vals[key][0];
-            max = vals[key][1];
-          } else min = vals[key];
-          return {
-            min,
-            max
-          };
+            let min;
+            let max;
+
+            if(vals[key] instanceof Array) {
+                min = vals[key][0];
+                max = vals[key][1];
+            } else {
+                min = vals[key];
+            }
+
+            return {
+                min,
+                max
+            };
         };
         const constraintPasses = (range, val) => {
-          if(typeof range.min !== "string") {
-            if(
-              +val < range.min ||
-              +val > range.max
-            ) {
-              return false;
+            if(typeof range.min !== "string") {
+                if(
+                    +val < range.min ||
+                    +val > range.max
+                ) {
+                    return false;
+                }
             }
-          }
-          return true;
+
+            return true;
         };
   
         const marketCapRange = defineConstraints(marketCap, filterValues.marketCap);
@@ -59,14 +67,26 @@ export default class Board extends React.Component {
         const volume_24hRange = defineConstraints(volume_24h, filterValues.volume_24h);
   
         const data = this.props.model.data.filter(item => {
-          return constraintPasses(marketCapRange, item.market_cap_usd) &&
-                 constraintPasses(priceRange, item.price_usd) &&
-                 constraintPasses(volume_24hRange, item["24h_volume_usd"]);
+          return constraintPasses(marketCapRange, item.market_cap_usd)
+                 && constraintPasses(priceRange, item.price_usd)
+                 && constraintPasses(volume_24hRange, item["24h_volume_usd"]);
         });
 
         this.setState({
             filteredData: data
         });
+    }
+    saveChanges() {
+        const {limit, currency} = this.props.model.filters;        
+
+        this.props.update(this.props.createURL(limit, currency), this.state.componentToUpdate)
+            .then(() => {
+                this.setState({
+                    filteredData: this.props.model.data
+                }, () => {
+                    this.updateTable();
+                });
+            });
     }
     clearFilters() {
         const filterNames = [ "marketCap", "price", "volume_24h" ];
@@ -78,42 +98,19 @@ export default class Board extends React.Component {
             filteredData: this.props.model.data
         });        
     }
-    changeTableCurrency(target) {
-        const newFilterValue = target.getAttribute("data-value");
-        const filterName = "currency";
-
-        this.saveChanges(newFilterValue, filterName);
-    }
-    changeTableLength(target) {
-        const newFilterValue = +(target.getAttribute("data-value"));
-        const filterName = "limit";
-
-        this.saveChanges(newFilterValue, filterName);
-    }
-    saveChanges(newFilterValue, filterName) {
-        const { limit, currency } = this.props.model.filters;
-        this.props.change(newFilterValue, filterName, this.state.componentToUpdate);             
-        this.props.update(this.props.createURL(limit, currency), this.state.componentToUpdate)
-        .then(() => {
-            this.setState({
-                filteredData: this.props.model.data
-            }, () => {
-                this.updateTable();
-            })
-        });
-    }   
     render() {
+        // const dataset = (!this.state.filteredData || Object.keys(this.state.filteredData).length === 0) ? [] : this.state.filteredData
         return (
             <div>
-                <Filters filterByMarketCap={this.changeFilter.bind(this, "marketCap")}
-                         filterByPrice={this.changeFilter.bind(this, "price")}
-                         filterByVolume_24h={this.changeFilter.bind(this, "volume_24h")}
-                         clearFilters={this.clearFilters.bind(this)}
-                         changeTableCurrency={this.changeTableCurrency.bind(this)}
-                         changeTableLength={this.changeTableLength.bind(this)}
+                <Filters filterByMarketCap={this.changeFilter.bind(this, this.updateTable, "marketCap")}
+                         filterByPrice={this.changeFilter.bind(this, this.updateTable, "price")}
+                         filterByVolume_24h={this.changeFilter.bind(this, this.updateTable, "volume_24h")}                         
+                         changeTableCurrency={this.changeFilter.bind(this, this.saveChanges, "currency")}
+                         changeTableLength={this.changeFilter.bind(this, this.saveChanges, "limit")}
+                         clearFilters={this.clearFilters}
                          filters={this.props.model.filters}
                 />
-                <Table dataset={ (!this.state.filteredData || Object.keys(this.state.filteredData).length === 0) ? [] : this.state.filteredData }
+                <Table dataset={this.state.filteredData || []}
                        currency={this.props.model.filters.currency}
                        onClickHandler={this.props.toggleCheckbox}
                        hashTable={this.props.hashTable}
