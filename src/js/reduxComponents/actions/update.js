05b72d404141cc7fb0823beb0e1dcd4850c44c73
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-07 20:56:40 +0200

    Enable requestData to handle synchronus requests

--- src/js/reduxComponents/actions/update.js
+++ src/js/reduxComponents/actions/update.js
@@ -1,4 +1,9 @@
-export function dataRequest(url, componentToUpdate) {  
+export function dataRequest(url, componentToUpdate, newData) {
+  // for synchronous changes, data is already provided via newData
+  if(!!newData && !url) {
+    return dispatch => dispatch(_updateData(newData, componentToUpdate)); // ?
+  }
+
   return dispatch => fetch(url)
     .then(response => response.json())
     .then(data => dispatch(_updateData(data, componentToUpdate)))
