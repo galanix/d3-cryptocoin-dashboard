26ff6b873edeeca607d591305cf80c056e1b84d9
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-07 21:00:06 +0200

    Add SavedGraphs component to navigation and Dashboard

--- src/js/reactComponents/Dashboard/Dashboard.js
+++ src/js/reactComponents/Dashboard/Dashboard.js
@@ -7,32 +7,30 @@ import CryptoBoard from './CryptoBoard/CryptoBoard';
 
 export default function Dashboard(props) {
   const displayComponent = props.data.settings.displayComponent;
-  return (
-    <div className="right_col" role="main">
-      <div className="row">
-        { displayComponent.BitcoinCurrentPrice ? 
-          <BitcoinCurrentPrice update={props.update}
-                               model={props.data.currentPrice}                                         
-                               signs={props.data.general.currencySigns} /> : null
-        }
-        { displayComponent.BitcoinHistoryGraph ?
-          <BitcoinHistoryGraph update={props.update}
-                               change={props.change}
-                               model={props.data.history}                                         
-                               signs={props.data.general.currencySigns} /> : null
-        }
-        { displayComponent.CurrencyPairGraph ?
-            <CurrencyPairGraph update={props.update}
-                               change={props.change}
-                               model={props.data.currencyPair} /> : null
-        }
-        { displayComponent.CryptoBoard ?
-            <CryptoBoard update={props.update}
-                         change={props.change}
-                         model={props.data.cryptoBoard}
-                         signs={props.data.general.currencySigns} /> : null
-        }             
-      </div>
+  return (    
+    <div className="row">
+      { displayComponent.BitcoinCurrentPrice ? 
+        <BitcoinCurrentPrice update={props.update}
+                              model={props.data.currentPrice}                                         
+                              signs={props.data.general.currencySigns} /> : null
+      }
+      { displayComponent.BitcoinHistoryGraph ?
+        <BitcoinHistoryGraph update={props.update}
+                              change={props.change}
+                              model={props.data.history}                                         
+                              signs={props.data.general.currencySigns} /> : null
+      }
+      { displayComponent.CurrencyPairGraph ?
+          <CurrencyPairGraph update={props.update}
+                              change={props.change}
+                              model={props.data.currencyPair} /> : null
+      }
+      { displayComponent.CryptoBoard ?
+          <CryptoBoard update={props.update}
+                        change={props.change}
+                        model={props.data.cryptoBoard}
+                        signs={props.data.general.currencySigns} /> : null
+      }             
     </div>
   );
 }
