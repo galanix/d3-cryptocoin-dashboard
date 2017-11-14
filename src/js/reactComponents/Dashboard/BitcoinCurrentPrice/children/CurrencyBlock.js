60312af0dd213f0bd3e5136e7017e522f3738f8e
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-14 17:12:08 +0200

    remove a warning message by preventing component from updating after it has been unmounted

--- src/js/reactComponents/Dashboard/BitcoinCurrentPrice/children/CurrencyBlock.js
+++ src/js/reactComponents/Dashboard/BitcoinCurrentPrice/children/CurrencyBlock.js
@@ -10,19 +10,19 @@ export default class CurrencyBlock extends React.Component {
   componentWillReceiveProps() {
     this.setState({
       blink: true,
-      timeoutId: setTimeout(() => this.setState({ blink: false }), 3000),
+      timeoutId: setTimeout(() => {
+        if(!!this.div) {
+          this.setState({ blink: false });
+        }
+      }, 3000)
     });
   }
-  componentWillUnmount() {
-    this.setState({
-      blink: false,
-    }, () => {
-      clearTimeout(this.state.timeoutId);
-    });
+  componentWillUnmount() {    
+    clearTimeout(this.state.timeoutId);
   }
   render() {
     return  (
-      <div className={`${this.props.classesCSS} col-md-3 col-sm-6 col-xs-6 tile_stats_count`}>
+      <div ref={div => this.div = div} className={`${this.props.classesCSS} col-md-3 col-sm-6 col-xs-6 tile_stats_count`}>
         <span className="count_top">{this.props.text}</span>
         <div className={`count ${this.state.blink ? 'blinking' : ''}`}  dangerouslySetInnerHTML={{__html: this.props.sign + this.props.currencyValue}}></div>
         <span className="count_bottom">
