9baa253e03f748415a2bb23eb36e10f3376e5590
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-17 16:44:04 +0200

    Clean indentation

--- src/js/reactComponents/Dashboard/BitcoinCurrentPrice/children/CurrencyBlock.js
+++ src/js/reactComponents/Dashboard/BitcoinCurrentPrice/children/CurrencyBlock.js
@@ -16,12 +16,16 @@ export default class CurrencyBlock extends React.Component {
         }
       }, 3000)
     });
-  } 
+  }
   render() {
     return  (
       <div ref={div => this.div = div} className={`${this.props.classesCSS} col-md-3 col-sm-6 col-xs-6 tile_stats_count`}>
         <span className="count_top">{this.props.text}</span>
-        <div className={`count ${this.state.blink ? 'blinking' : ''}`}  dangerouslySetInnerHTML={{__html: this.props.sign + this.props.currencyValue}}></div>
+        <div 
+          className={`count ${this.state.blink ? 'blinking' : ''}`}  
+          dangerouslySetInnerHTML={{__html: this.props.sign + this.props.currencyValue}}
+        >
+        </div>
         <span className="count_bottom">
           {this.props.renderChange()}
         </span>
