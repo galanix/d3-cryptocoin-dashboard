8348fb62d6d0c861dd1d97ae619baff7d0c967b0
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-17 16:50:31 +0200

    Clean indentation of Button Group component

--- src/js/reactComponents/General/ButtonGroup.js
+++ src/js/reactComponents/General/ButtonGroup.js
@@ -1,48 +1,44 @@
 import React from 'react';
 
-class ButtonGroup extends React.Component {
-    constructor() {
-        super();
-        this.state = { 
-            prevClickedBtn: null
-        };
-    }
-    componentDidMount() {
-        if(this.props.noSingleButtonSelection) return;
+export default class ButtonGroup extends React.Component { 
+  componentDidMount() {
+    if(this.props.noSingleButtonSelection) return;
 
-        this.setState({
-            prevClickedBtn: this.container.querySelector('.active')
-        });
-    }
-    handleClick(evt) {
-        const target = evt.target;
-        if(target === this.state.prevClickedBtn) return;
+    this.setState({
+      prevClickedBtn: this.container.querySelector('.active')
+    });
+  }
+  handleClick(evt) {
+    const target = evt.target;
+    if(target === this.state.prevClickedBtn) return;
 
-        if(!!this.state.prevClickedBtn && target.tagName === 'BUTTON') {
-            this.state.prevClickedBtn.classList.remove('active');
-            this.setState({
-                prevClickedBtn: target
-            }, () => this.state.prevClickedBtn.classList.add('active'));
-        }
-
-        this.props.onClickHandler(target);
+    if(!!this.state.prevClickedBtn && target.tagName === 'BUTTON') {
+      this.state.prevClickedBtn.classList.remove('active');
+      this.setState({
+        prevClickedBtn: target
+      }, () => this.state.prevClickedBtn.classList.add('active'));
     }
-    render() {
-        return (
-            <div ref={div => this.container = div}
-                 className={this.props.classesCSS}
-                 onClick={evt => this.handleClick(evt)}
-                 {...this.props.containerAttrs}
+
+    this.props.onClickHandler(target);
+  }
+  render() {
+    return (
+      <div
+        ref={div => this.container = div}
+        className={this.props.classesCSS}
+        onClick={evt => this.handleClick(evt)}
+        {...this.props.containerAttrs}
+      >
+        {this.props.buttons.map((btn, index) => (
+            <button
+              key={index} {...btn.attrs }
+              className={`btn ${btn.classesCSS || ''}`}
+              id={btn.id}
             >
-                {this.props.buttons.map((btn, index) => (
-                    <button key={index} {...btn.attrs }
-                            className={`btn ${btn.classesCSS}`}
-                            id={btn.id}>{btn.textValue}                    
-                    </button>
-                ))}
-            </div>
-        );
-    }
+              {btn.textValue}
+            </button>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default ButtonGroup;
