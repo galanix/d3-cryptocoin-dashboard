5eda7569eefc88158be6e29bd281827f1b21c811
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-20 14:12:21 +0200

    Refactor code to generate th elements instead of hard-coding them and fire onClick event for th elements

--- src/js/reactComponents/Dashboard/CryptoBoard/children/children/Table.js
+++ src/js/reactComponents/Dashboard/CryptoBoard/children/children/Table.js
@@ -1,54 +1,83 @@
-import React from "react";
+import React from 'react';
 
-export default function Table(props) {
+export default class Table extends React.Component {
   // data-sort-by values are properties in a single item of dataset
-  return (
-    <div className="row">
-      <div className="col-xs-12">
-        <div className="table-responsive">
-          <table id="datatable-checkbox" className="table table-striped table-bordered bulk_action">
-            <thead>
-              <tr onClick={evt => props.sortTable(evt)} >
-                <th>
-                  <i className="fa fa-check"></i>
-                </th>
-                <th>#</th>
-                <th data-sort-by="name">Name</th>
-                <th data-sort-by="market_cap">Market Cap</th>
-                <th data-sort-by="price">Price</th>
-                <th data-sort-by="available_supply">Circulating Supply</th>
-                <th data-sort-by="24h_volume">Volume(24h)</th>
-                <th data-sort-by="percent_change_1h">%1h</th>
-                <th data-sort-by="percent_change_24h">%24h</th>
-                <th data-sort-by="percent_change_7d">%7d</th>
-              </tr>
-            </thead>
-            <tbody onClick={evt => props.onClickHandler(evt)}>
-              {props.dataset.map((item, index) => (
-                <tr key={index}>
-                  <td data-toggle="button">
-                    <button 
-                      className={`btn btn-xs btn-dark ${(!props.hashTable || !props.hashTable[item.id]) ? '' : 'active'}`}
-                      data-currency-id={item.id}
-                    >
-                      <span className="fa fa-check"></span>
-                    </button>
-                  </td>
-                  <td>{index + 1}</td>
-                  <td>{item.name}</td>
-                  <td>{item["market_cap_" + props.currency.toLowerCase()]}</td>
-                  <td>{(+item["price_" + props.currency.toLowerCase()]).toFixed(5)}</td>
-                  <td>{item.available_supply}</td>
-                  <td>{item["24h_volume_" + props.currency.toLowerCase()]}</td>
-                  <td>{item.percent_change_1h}</td>
-                  <td>{item.percent_change_24h}</td>
-                  <td>{item.percent_change_7d}</td>
+  constructor() {
+    super();
+    this.state = {
+      name: 'name',
+      marketCap: 'market_cap_',
+      price: 'price_',
+      availableSupply: 'available_supply',
+      volume: '24h_volume_',
+      percentChange_1Hour: 'percent_change_1h',
+      percentChange_24Hours: 'percent_change_24h',
+      percentChange_7Days: 'percent_change_7d',
+    }
+  }
+  generateTH(colVal, textVal) {
+    return (
+      <th data-sort-by={colVal} key={colVal}>
+        { this.props.colToSort === colVal ? this.props.sortOrderIcon : null }
+        { textVal }
+      </th>
+    );
+  }
+  render() {
+    const thElements = {
+      'Name': this.state.name,
+      'Market Cap': this.state.marketCap,
+      'Price': this.state.price,
+      'Circulating Supply': this.state.availableSupply,
+      'Volume(24h)': this.state.volume,
+      '%1h': this.state.percentChange_1Hour,
+      '%24h': this.state.percentChange_24Hours,
+      '%7d': this.state.percentChange_7Days,
+    };
+
+    return (
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="table-responsive">
+            <table id="datatable-checkbox" className="table table-striped table-bordered bulk_action">
+              <thead>
+                <tr onClick={evt => this.props.sortTable(evt)} >
+                  <th>
+                    <i className="fa fa-check"></i>
+                  </th>
+                  <th>#</th>
+
+                  { Object.keys(thElements).map(key => this.generateTH(thElements[key], key)) }
+                  
                 </tr>
-                ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody onClick={evt => this.props.onClickHandler(evt)}>
+                {this.props.dataset.map((item, index) => (
+                  <tr key={index}>
+                    <td data-toggle="button">
+                      <button 
+                        className={`btn btn-xs btn-dark ${(!this.props.hashTable || !this.props.hashTable[item.id]) ? "" : "active"}`}
+                        data-currency-id={item.id}
+                      >
+                        <span className="fa fa-check"></span>
+                      </button>
+                    </td>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>{item["market_cap_" + this.props.currency.toLowerCase()]}</td>
+                    <td>{(+item["price_" + this.props.currency.toLowerCase()]).toFixed(5)}</td>
+                    <td>{item.available_supply}</td>
+                    <td>{item["24h_volume_" + this.props.currency.toLowerCase()]}</td>
+                    <td>{item.percent_change_1h}</td>
+                    <td>{item.percent_change_24h}</td>
+                    <td>{item.percent_change_7d}</td>
+                  </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
