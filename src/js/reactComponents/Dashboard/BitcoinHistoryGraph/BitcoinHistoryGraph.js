cd91cc00d07d8843509ad37c2386cf6b4514aec4
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-24 15:53:42 +0200

    Move calendar widget event handling from CalendarForm component into BitcoinHistoryGraph, add error message when the CalendarForm input is invalid and reflect timeline changes from button group onto input CalendarForm

--- src/js/reactComponents/Dashboard/BitcoinHistoryGraph/BitcoinHistoryGraph.js
+++ src/js/reactComponents/Dashboard/BitcoinHistoryGraph/BitcoinHistoryGraph.js
@@ -6,6 +6,7 @@ import Dropdown from '../../General/Dropdown';
 import CalendarForm from '../../General/CalendarForm';
 import ButtonGroup from '../../General/ButtonGroup';
 import LineChart from './children/LineChart';
+import Message from '../../General/Message';
 
 // HELPER FUNCTIONS
 import { formProperDateFormat, scaleGraphSize } from '../../../helperFunctions';
@@ -14,13 +15,34 @@ export default class BitcoinHistoryGraph extends React.Component {
   constructor() {
     super();
     this.state = {
-      componentToUpdate: 'BitcoinHistoryGraph'
-    };
+      componentToUpdate: 'BitcoinHistoryGraph',      
+    };        
+
+    this.createURL = this.createURL.bind(this);
+    this.renderGraph = this.renderGraph.bind(this);
+    this.timelineFilterChange = this.timelineFilterChange.bind(this);
+    this.currencyFilterChange = this.currencyFilterChange.bind(this);
+    this.onWidgetChange = this.onWidgetChange.bind(this);
+
   }
   componentDidMount() {
+    this.hideErrorMsg();
+
+    const { start, end } = this.props.model.filters;
+    this.setState({
+      startPlaceholder: 'From: ' + start,
+      endPlaceholder: 'To: ' + end,
+    });
+
     this.props.update(this.createURL(), this.state.componentToUpdate)
       .then(() => this.renderGraph(false))
-      .catch(err => { console.warn('failed fetch') });
+      .catch(err => { console.warn('failed fetch', err) });
+  }
+  hideErrorMsg() {
+    this.Message.hide();
+  }
+  showErrorMsg() {
+    this.Message.show();  
   }
   createURL() {
     const { start, end, currency } = this.props.model.filters;
@@ -28,10 +50,14 @@ export default class BitcoinHistoryGraph extends React.Component {
     return url + `?start=${start}&end=${end}&currency=${currency}`;
   }
   renderGraph(componentIsUpdated) {
-    if(!this.props.model.data) return;
+    if(!this.props.model.data) {
+      return;
+    }
+
+    this.hideErrorMsg();
+
     // transforms a string into a Date object
     // create an array(dataset) from an object(data)
-
     const dataset = [];
     const data = this.props.model.data.bpi;
     Object.keys(data).forEach(key => {
@@ -51,15 +77,18 @@ export default class BitcoinHistoryGraph extends React.Component {
       .then(() => {
         this.renderGraph(true);
         this.chart.hidePreloader();
-      });
+      })
+      .catch(err => console.log(err));
   }
   timelineFilterChange(target) {
-    if(target.tagName !== 'BUTTON') return;
+    if(target.tagName !== 'BUTTON') {
+      return;
+    }
     
     const btnValue = target.getAttribute('data-timeline'); // button value
     const today = new Date(); // endDate
     const startDate = new Date();
-    let timeline; // each of 6 buttons fall under 3 periods   
+    let timeline; // each of 6 buttons fall under 3 periods
   
     switch(btnValue) {
       case 'all-time':
@@ -69,19 +98,19 @@ export default class BitcoinHistoryGraph extends React.Component {
         timeline = 'from-all-time-to-year';
         break;
       case '1-year':
-        startDate.setFullYear(startDate.getFullYear() - 1)
+        startDate.setFullYear(startDate.getFullYear() - 1);
         timeline = 'from-year-to-3-month';
         break;
       case '6-month':
-        startDate.setMonth(startDate.getMonth() - 6)
+        startDate.setMonth(startDate.getMonth() - 6);
         timeline = 'from-year-to-3-month';
         break;
       case '3-month':
-        startDate.setMonth(startDate.getMonth() - 3)
+        startDate.setMonth(startDate.getMonth() - 3);
         timeline = 'less-than-3-month';
         break;
       case '1-month':      
-        startDate.setMonth(startDate.getMonth() - 1)
+        startDate.setMonth(startDate.getMonth() - 1);
         timeline ='less-than-3-month';
         break;
       case '1-week':
@@ -92,14 +121,22 @@ export default class BitcoinHistoryGraph extends React.Component {
         console.warn('unknown timeline: ', btnValue);
     }
 
-    const filterNames = [ 'currentTimeline', 'end', 'start' ];
+    const filterNames = [ 'currentTimeline', 'start', 'end' ];    
+    const startFilterValue = formProperDateFormat(startDate.getFullYear(), startDate.getMonth() + 1, startDate.getDate());
+    const endFilterValue = formProperDateFormat(today.getFullYear(), today.getMonth() + 1, today.getDate());    
+
     const newFilterValues = [
       timeline,
-      formProperDateFormat(today.getFullYear(), today.getMonth() + 1, today.getDate()),
-      formProperDateFormat(startDate.getFullYear(), startDate.getMonth() + 1, startDate.getDate())
-    ];    
-   
+      startFilterValue,
+      endFilterValue
+    ];
+
     this.saveChangesAndRerender(newFilterValues, filterNames);
+
+    this.setState({
+      startPlaceholder: 'From: ' + startFilterValue,
+      endPlaceholder: 'To: ' + endFilterValue,
+    });
   }
   currencyFilterChange(target) {
     const newFilterValue = target.getAttribute('data-value');
@@ -107,80 +144,137 @@ export default class BitcoinHistoryGraph extends React.Component {
 
     this.saveChangesAndRerender(newFilterValue, filterName)
   }
+  onWidgetChange({ inputId, selectedDates, dateStr }) {
+    let filterName;
+    let startDate;
+    let endDate;
+
+    if(inputId === "start") {
+      this.setState({
+        startPlaceholder: "From: " + dateStr
+      });
+
+      filterName = "start";
+      startDate = new Date(dateStr);
+      endDate = new Date(this.props.model.filters.end);
+
+    } else { // === end
+      this.setState({
+        endPlaceholder: "To: " + dateStr
+      });
+
+      filterName = "end";
+      startDate = new Date(this.props.model.filters.start);
+      endDate = new Date(dateStr);
+    }
+
+    if(endDate.getTime() > startDate.getTime()) {      
+
+      let timeline;
+      const monthDiff = endDate.getMonth() - startDate.getMonth();
+      switch(monthDiff) {
+        case 0: case 1: case 2: case 3:
+          timeline = "less-than-3-month";
+          break;
+
+        default:
+          timeline = "from-year-to-3-month";
+      }
+      const yearDiff = endDate.getFullYear() - startDate.getFullYear();
+      if(yearDiff > 0) {
+        timeline = "from-all-time-to-year";
+      }
+      
+      const filterNames = [ "currentTimeline", filterName, ];
+      const newFilterValues = [ timeline, dateStr, ];
+      this.saveChangesAndRerender(newFilterValues, filterNames);
+
+    } else {
+      this.showErrorMsg();
+    }
+
+  }  
   render() {
     return (
-        <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-            <section id="history" className="row x_panel">
-                <Header classesCSS="col-md-12 col-sm-12 col-xs-12 x_title"
-                        titleText="Bitcoin History"
-                />
-                <div className="col-md-12 col-sm-12 col-xs-12">
-                    <Dropdown onClickHandler={this.currencyFilterChange.bind(this)}
-                              classesCSS={{
-                                  container: "well",
-                                  button: "btn-success"
-                              }}
-                              titleText="Currency"
-                              defaultDataValue={this.props.model.filters.currency}
-                              options={[
-                                  { dataValue: "USD" },
-                                  { dataValue: "EUR" },
-                                  { dataValue: "RUB" },
-                                  { dataValue: "UAH" },
-                              ]}
-                    />
-                    <div className="well" style={{ "overflow" : "auto"}}>
-                        <CalendarForm name="start" id="start"
-                                      start={this.props.model.filters.start}
-                                      end={this.props.model.filters.end}
-                                      change={this.props.change}
-                                      update={this.props.update}
-                                      createURL={this.createURL.bind(this)}
-                                      renderGraph={this.renderGraph.bind(this)}/>
-                        <CalendarForm name="end" id="end"
-                                      start={this.props.model.filters.start}
-                                      end={this.props.model.filters.end}
-                                      change={this.props.change}
-                                      update={this.props.update}
-                                      createURL={this.createURL.bind(this)}
-                                      renderGraph={this.renderGraph.bind(this)}/>
-                    </div>
-                    <ButtonGroup onClickHandler={this.timelineFilterChange.bind(this)}
-                                 classesCSS="well btn-group full-width"                                 
-                                 buttons={[
-                                   { attrs: { "data-timeline": "all-time" },
-                                     classesCSS:"btn-success",
-                                     textValue: "All time"
-                                   },
-                                   { attrs: { "data-timeline": "1-year" },
-                                     classesCSS: "btn-success",
-                                     textValue: "Year"
-                                   },
-                                   { attrs: { "data-timeline": "6-month" },
-                                     classesCSS: "btn-success",
-                                     textValue: "6 months"
-                                   },
-                                   { attrs: { "data-timeline": "3-month" },
-                                     classesCSS: "btn-success",
-                                     textValue: "3 months"
-                                   },
-                                   { attrs: { "data-timeline": "1-month" },
-                                     classesCSS: "btn-success active",
-                                     textValue: "1 month"
-                                   },
-                                   { attrs: { "data-timeline": "1-week" },
-                                     classesCSS: "btn-success",
-                                     textValue: "week"
-                                    },
-                                 ]}
-                    />
-                </div>
-                <LineChart ref={lineChart => this.chart = lineChart}
-                           model={this.props.model}
-                           signs={this.props.signs}
-                />
-            </section>
-        </div>
+      <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+        <section id="history" className="row x_panel">
+          <Header
+            classesCSS="col-md-12 col-sm-12 col-xs-12 x_title"
+            titleText="Bitcoin History"
+          />
+          <div className="col-md-12 col-sm-12 col-xs-12">
+            <Dropdown
+              onClickHandler={this.currencyFilterChange}
+              classesCSS={{
+                container: "well",
+                button: "btn-success"
+              }}
+              titleText="Currency"
+              defaultDataValue={this.props.model.filters.currency}
+              options={[
+                { dataValue: "USD" },
+                { dataValue: "EUR" },
+                { dataValue: "RUB" },
+                { dataValue: "UAH" },
+              ]}
+            />
+            <div className="well" style={{ "overflow" : "auto"}}>
+              <CalendarForm
+                name="start" 
+                id="start"                
+                placeholder={this.state.startPlaceholder}                
+                onWidgetChange={this.onWidgetChange}
+              />
+              <CalendarForm 
+                name="end" 
+                id="end"
+                placeholder={this.state.endPlaceholder}                
+                onWidgetChange={this.onWidgetChange}
+              />
+              <Message
+                ref={Message => this.Message = Message}
+                additionalClasses="error"
+                msg="An error has occured..."
+              />
+            </div>
+            <ButtonGroup 
+              onClickHandler={this.timelineFilterChange}
+              classesCSS="well btn-group full-width"                                 
+              buttons={[
+                { attrs: { "data-timeline": "all-time" },
+                  classesCSS:"btn-success",
+                  textValue: "All time"
+                },
+                { attrs: { "data-timeline": "1-year" },
+                  classesCSS: "btn-success",
+                  textValue: "Year"
+                },
+                { attrs: { "data-timeline": "6-month" },
+                  classesCSS: "btn-success",
+                  textValue: "6 months"
+                },
+                { attrs: { "data-timeline": "3-month" },
+                  classesCSS: "btn-success",
+                  textValue: "3 months"
+                },
+                { attrs: { "data-timeline": "1-month" },
+                  classesCSS: "btn-success active",
+                  textValue: "1 month"
+                },
+                { attrs: { "data-timeline": "1-week" },
+                  classesCSS: "btn-success",
+                  textValue: "week"
+                },
+              ]}
+            />
+          </div>
+          <LineChart 
+            ref={lineChart => this.chart = lineChart}
+            model={this.props.model}
+            signs={this.props.signs}
+          />
+        </section>
+      </div>
     );
   }
 };
