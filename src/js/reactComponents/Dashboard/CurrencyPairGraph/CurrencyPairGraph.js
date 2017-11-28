2a1bc314b103646b3ad365276c8878c6f3a8d740
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-28 18:57:57 +0200

    Add additional button for submiting hours filter and refactor code according to eslint

--- src/js/reactComponents/Dashboard/CurrencyPairGraph/CurrencyPairGraph.js
+++ src/js/reactComponents/Dashboard/CurrencyPairGraph/CurrencyPairGraph.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Header from '../../General/Header';
 import Dropdown from '../../General/Dropdown';
@@ -7,10 +8,7 @@ import ButtonGroup from '../../General/ButtonGroup';
 import LineChart from './children/AugmentedBasicLineChart';
 import Message from '../../General/Message';
 
-
-import { scaleGraphSize } from '../../../helperFunctions';
-
-export default class CurrencyPairGraph extends React.Component {
+class CurrencyPairGraph extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -25,25 +23,27 @@ export default class CurrencyPairGraph extends React.Component {
   }
   componentDidMount() {
     this.props.update(this.createURL(), this.state.componentToUpdate)
-    .then(() => this.renderGraphs(false))
-    .catch(err => console.warn(err));
+      .then(() => this.renderGraphs(false))
+      .catch((err) => { throw new Error(err); });
   }
   createURL() {
-    const { pairName, dataPoints, hours } = this.props.model.filters;    
+    const { pairName, dataPoints, hours } = this.props.model.filters;
     return `https://api.nexchange.io/en/api/v1/price/${pairName}/history/?data_points=${dataPoints}&format=json&hours=${hours}`;
   }
-  renderGraphs(isModuleBeingUpdated) {    
-    if(isModuleBeingUpdated) {
-      // substitute dataset and update current graphs
-      this.charts.updateLine(this.props.model.data);
-    } else {
-      // build new graphs from scratch and add event listeners for filters
-      this.charts.buildLine(this.props.model.data);
-    }
+  toggleGraphs(target) {
+    let targetBtn = target;
+    if (targetBtn.tagName === 'SPAN') targetBtn = targetBtn.parentElement;
+    const { id } = targetBtn;
+
+    const active = targetBtn.classList.contains('active');
+    if (!active) targetBtn.classList.add('active');
+    else targetBtn.classList.remove('active');
+
+    this.charts.toggleGraphs(id, active);
   }
   saveChangesAndRerender(newFilterValue, filterName) {
     this.charts.showMessage();
-    this.props.change(newFilterValue, filterName, this.state.componentToUpdate)
+    this.props.change(newFilterValue, filterName, this.state.componentToUpdate);
     this.props.update(this.createURL(), this.state.componentToUpdate)
       .then(() => {
         this.renderGraphs(true);
@@ -52,44 +52,46 @@ export default class CurrencyPairGraph extends React.Component {
   }
   currencyFilterChange(target) {
     const filterName = 'pairName';
-    const newFilterValue = target.getAttribute('data-value');        
-        
+    const newFilterValue = target.getAttribute('data-value');
     this.saveChangesAndRerender(newFilterValue, filterName);
   }
   frequencyFilterChange(target) {
     const frequency = target.getAttribute('data-value') || '';
-    const hours = this.props.model.filters.hours;
+    const { hours } = this.props.model.filters;
     const divisor = this.props.model.dataPointDivisors[frequency];
 
     let dataPoints = Math.floor(hours / divisor);
-    if(dataPoints !== 0) {
-      if(dataPoints === 1) {
-        dataPoints++;
-      }        
+    if (dataPoints !== 0) {
+      if (dataPoints === 1) {
+        dataPoints += 1;
+      }
 
       const filterNames = ['dataPoints', 'currentDivisor', 'frequency'];
       const newFilterValues = [dataPoints, divisor, frequency];
-    
       this.saveChangesAndRerender(newFilterValues, filterNames);
     }
   }
   hoursFilterChange(evt) {
-    const input = evt.target.querySelector('input');
+    let { target } = evt;
+    if (target.tagName === 'BUTTON') {
+      target = target.parentElement;
+    }
+
+    const input = target.querySelector('input');
     const hours = parseFloat(input.value);
 
-    if(!hours) {
+    if (!hours) {
       this.showError();
       return;
     }
-    
     this.hideError();
 
-    const divisor = this.props.model.filters.currentDivisor;        
-    const dataPoints = Math.floor(hours / divisor);        
-    const filterNames = ['dataPoints', 'hours'];        
+    const divisor = this.props.model.filters.currentDivisor;
+    const dataPoints = Math.floor(hours / divisor);
+    const filterNames = ['dataPoints', 'hours'];
     const newFilterValues = [dataPoints, hours];
 
-    input.placeholder = hours + ' Hours'
+    input.placeholder = `${hours} Hours`;
     input.value = '';
     input.blur();
 
@@ -107,16 +109,14 @@ export default class CurrencyPairGraph extends React.Component {
       isFormHighlighted: false,
     });
   }
-  toggleGraphs(target) {
-    let targetBtn = target;
-    if(targetBtn.tagName === 'SPAN') targetBtn = targetBtn.parentElement;        
-    const id = targetBtn.id;
-
-    const active = targetBtn.classList.contains('active');
-    if(!active) targetBtn.classList.add('active');
-    else targetBtn.classList.remove('active');
-
-    this.charts.toggleGraphs(id, active);
+  renderGraphs(isModuleBeingUpdated) {
+    if (isModuleBeingUpdated) {
+      // substitute dataset and update current graphs
+      this.charts.updateLine(this.props.model.data);
+    } else {
+      // build new graphs from scratch and add event listeners for filters
+      this.charts.buildLine(this.props.model.data);
+    }
   }
   render() {
     return (
@@ -129,38 +129,38 @@ export default class CurrencyPairGraph extends React.Component {
           <div className="col-md-12 col-sm-12 col-xs-12">
             <div className="well dropdown-group">
               <Dropdown
-                classesCSS={{ button: "btn-success", dropdown: "dropdown_currency" }}
+                classesCSS={{ button: 'btn-success', dropdown: 'dropdown_currency' }}
                 titleText="Currency"
                 onClickHandler={this.currencyFilterChange}
                 defaultDataValue={this.props.model.filters.pairName}
                 options={[
-                  { dataValue:"BTCLTC", textValue: "BTC - LTC" },
-                  { dataValue:"LTCBTC", textValue: "LTC - BTC" },
-                  { dataValue:"ETHBTC", textValue: "ETH - BTC" },
-                  { dataValue:"BTCETH", textValue: "BTC - ETH" },
-                  { dataValue:"LTCETH", textValue: "LTC - ETH" },
-                  { dataValue:"ETHLTC", textValue: "ETH - LTC" },
+                  { dataValue: 'BTCLTC', textValue: 'BTC - LTC' },
+                  { dataValue: 'LTCBTC', textValue: 'LTC - BTC' },
+                  { dataValue: 'ETHBTC', textValue: 'ETH - BTC' },
+                  { dataValue: 'BTCETH', textValue: 'BTC - ETH' },
+                  { dataValue: 'LTCETH', textValue: 'LTC - ETH' },
+                  { dataValue: 'ETHLTC', textValue: 'ETH - LTC' },
                 ]}
               />
-              <Dropdown 
-                classesCSS={{ button: "btn-success", dropdown: "dropdown_frequency" }}
+              <Dropdown
+                classesCSS={{ button: 'btn-success', dropdown: 'dropdown_frequency' }}
                 onClickHandler={this.frequencyFilterChange}
                 titleText="Frequencies"
                 defaultDataValue={this.props.model.filters.frequency}
                 options={[
-                  { dataValue:"1 min" },
-                  { dataValue:"5 mins" },
-                  { dataValue:"30 mins" },
-                  { dataValue:"1 hour" },
-                  { dataValue:"3 hours" },
-                  { dataValue:"6 hours" },
-                  { dataValue:"12 hours" },
-                  { dataValue:"24 hours" },
+                  { dataValue: '1 min' },
+                  { dataValue: '5 mins' },
+                  { dataValue: '30 mins' },
+                  { dataValue: '1 hour' },
+                  { dataValue: '3 hours' },
+                  { dataValue: '6 hours' },
+                  { dataValue: '12 hours' },
+                  { dataValue: '24 hours' },
                 ]}
               />
             </div>
-            <div className="well" style={{"overfow": "auto"}}>
-              <div className="col-md-12 col-sm-12 col-xs-12">
+            <div className="well" style={{ overfow: 'auto' }}>
+              <div className="col-md-12 col-sm-12 col-xs-12 InputFormContainer">
                 <InputForm
                   isFormHighlighted={this.state.isFormHighlighted}
                   formCSSClasses="form-horizontal form-label-left input_mask"
@@ -170,44 +170,53 @@ export default class CurrencyPairGraph extends React.Component {
                   inputIcon="fa fa-clock-o"
                   onSubmitHandler={this.hoursFilterChange}
                 />
+                <button className="btn" onClick={this.hoursFilterChange}>Apply</button>
               </div>
               <Message
                 isMessageVisible={this.state.isErrorMessageVisible}
-                msg="Invalid input, try numbers"                
+                msg="Invalid input, try numbers"
                 CSSClasses="error"
               />
-              <div className="clearfix"></div>
+              <div className="clearfix" />
             </div>
             <div className="well toggle-graphs">
-              <ButtonGroup 
-                containerAttrs={{ "data-toggle": "buttons"}}
+              <ButtonGroup
+                containerAttrs={{ 'data-toggle': 'buttons' }}
                 classesCSS="btn-group"
-                noSingleButtonSelection={true}
+                noSingleButtonSelection
                 onClickHandler={this.toggleGraphs}
-                buttons={[
-                  { classesCSS: "btn-info active",
-                    id: "ask",
-                    textValue: ["Ask ", <span key="0" className="glyphicon glyphicon-ok"></span>]
+                buttons={[{
+                    classesCSS: 'btn-info active',
+                    id: 'ask',
+                    textValue: ['Ask ', <span key="0" className="glyphicon glyphicon-ok" />],
+                  }, {
+                    classesCSS: 'btn-danger active',
+                    id: 'bid',
+                    textValue: ['Bid ', <span key="1" className="glyphicon glyphicon-ok" />],
+                  }, {
+                    classesCSS: 'btn-success',
+                    id: 'spread',
+                    textValue: ['Spread ', <span key="2" className="glyphicon glyphicon-ok" />],
                   },
-                  { classesCSS: "btn-danger active",
-                    id: "bid",
-                    textValue: ["Bid ", <span key="1" className="glyphicon glyphicon-ok"></span>]
-                  },
-                  { classesCSS: "btn-success",
-                    id: "spread",
-                    textValue: ["Spread ", <span key="2" className="glyphicon glyphicon-ok"></span>]
-                  }
                 ]}
               />
             </div>
           </div>
           <LineChart
-            ref={lineCharts => this.charts = lineCharts}
+            ref={(lineCharts) => { this.charts = lineCharts; }}
             model={this.props.model}
-            graphId="ask-bid-spread"                            
+            graphId="ask-bid-spread"
           />
         </section>
       </div>
     );
   }
+}
+
+CurrencyPairGraph.propTypes = {
+  update: PropTypes.func,
+  change: PropTypes.func,
+  model: PropTypes.object,
 };
+
+export default CurrencyPairGraph;
