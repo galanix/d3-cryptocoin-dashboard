a04f5146e7ee5cb967db6588ead63ed83c7c80e8
VladTarGalanix <vlad.galanix@gmail.com>
2017-11-28 18:50:19 +0200

    Add linter configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": false,
+        "react/require-default-props": false,
+        "react/forbid-prop-types": false,
+        "react/sort-comp": false,
+    },
+    "env": {
+        "browser": true
+    },
+};
