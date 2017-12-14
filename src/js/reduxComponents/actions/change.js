28cc264b2e218a25ddb54c8e0c3642bd0e814b8e
VladTarGalanix <vlad.galanix@gmail.com>
2017-12-14 22:00:18 +0200

    use export default instead on export

--- src/js/reduxComponents/actions/change.js
+++ src/js/reduxComponents/actions/change.js
@@ -1,10 +1,10 @@
-export function filterChange(newFilterValue, filterName, componentToUpdate) {
-  return dispatch => {
+export default function filterChange(newFilterValue, filterName, componentToUpdate) {
+  return (dispatch) => {
     dispatch({
       type: 'CHANGE_FILTERS',
       forComponent: componentToUpdate,
       filterName,
-      newFilterValue,      
+      newFilterValue,
     });
   };
 }
