646d8d3e4da22dff51603434add390b52a5eab78
VladTarGalanix <vlad.galanix@gmail.com>
2017-12-14 21:46:34 +0200

    Change false to 0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
     "extends": "airbnb",
     "rules": {
-        "react/jsx-filename-extension": false,
-        "react/require-default-props": false,
-        "react/forbid-prop-types": false,
-        "react/sort-comp": false,
+        "react/jsx-filename-extension": 0,
+        "react/require-default-props": 0,
+        "react/forbid-prop-types": 0,
+        "react/sort-comp": 0,
     },
     "env": {
         "browser": true
