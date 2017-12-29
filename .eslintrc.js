b6215ac83d39734308b2da430c43a4387f10f843
VladTarGalanix <vlad.galanix@gmail.com>
2017-12-29 18:51:19 +0200

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,9 @@ module.exports = {
         "react/sort-comp": 0,
         "class-methods-use-this": 0,
         "react/prop-types": 0,
+        "jsx-a11y/no-static-element-interactions": 0,
+        "jsx-a11y/click-events-have-key-events": 0,
+        "no-multi-spaces": 0
     },
     "env": {
         "browser": true
